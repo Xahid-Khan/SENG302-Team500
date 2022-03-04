f5dd17f17877aca6a4ff25f82db41a58ae901a7a
Zahid Khan <zkh22@uclive.ac.nz>
2022-03-04 13:31:43 +1300

    changed the data type in project and sprint respositories.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/repository/ProjectRepository.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/repository/ProjectRepository.java
@@ -3,6 +3,9 @@ package nz.ac.canterbury.seng302.portfolio.repository;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProjectRepository extends CrudRepository<Project, Long> {
-    Project findById(long id);
-}
+/**
+ * this interface extends the CRUD repository and makes use of the function/methods provided by the library.
+ */
+
+
+public interface ProjectRepository extends CrudRepository<Project, String> {}
