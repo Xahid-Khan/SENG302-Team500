f42443966541418e94a98479f104d0a214c1b3ab
Priscilla Ishida-Foale <pis19@uclive.ac.nz>
2022-03-04 14:16:35 +1300

    Moved scripts into its own script directory. Reformatted Thymeleaf syntax.

---
+++ portfolio/src/main/resources/static/scripts/indexjs.js
@@ -0,0 +1,17 @@
+/*
+    Adapted from https://www.w3schools.com/howto/howto_js_collapsible.asp
+     */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = document.getElementById("content");
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
