f429cb56a2b3ffc37e38c2326909a2e8f57c34c2
george <gca73@uclive.ac.nz>
2022-03-04 16:36:45 +1300

    Created controller for registration

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/RegistrationController.java
@@ -0,0 +1,24 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import io.grpc.StatusRuntimeException;
+import nz.ac.canterbury.seng302.portfolio.authentication.CookieUtil;
+import nz.ac.canterbury.seng302.portfolio.service.AuthenticateClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthenticateResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/register")//set path
+    public String register(Model model){//return data to the view using model
+        model.addAttribute("message","Register");//add data to the model
+        return "register";//return the template in templates folder
+    }
+}
