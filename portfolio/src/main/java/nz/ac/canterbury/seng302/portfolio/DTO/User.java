8b5c9504c9daea54530848b491fbc77348aa9c82
george <gca73@uclive.ac.nz>
2022-03-07 23:24:34 +1300

    Created DTO object for the thymeleaf form input

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/DTO/User.java
@@ -0,0 +1,14 @@
+package nz.ac.canterbury.seng302.portfolio.DTO;
+
+public class User {
+    private String username;
+    private String password;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String nickname;
+    private String bio;
+    private String pronouns;
+    private String email;
+
+}
