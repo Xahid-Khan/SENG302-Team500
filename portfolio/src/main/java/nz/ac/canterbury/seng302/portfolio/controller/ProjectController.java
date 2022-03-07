5147fa245f48c6a5ac7ddab28b913ce50caf558b
Xahid-Khan <xahidkhan88@gmail.com>
2022-03-07 16:47:50 +1300

    Added documentation to the Project Controller.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ProjectController.java
@@ -2,18 +2,19 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.model.contract.BaseProjectContract;
 import nz.ac.canterbury.seng302.portfolio.model.contract.ProjectContract;
-import nz.ac.canterbury.seng302.portfolio.model.entity.ProjectEntity;
 import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
-import org.h2.util.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
+
+
+/**
+ * This project controller file allows users to make API calls, such as GET, POST, PUT, DELETE requests.
+ *
+ */
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,6 +22,10 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    /**
+     * This method will be invoked when API receives a GET request, and will produce a list of all the projects.
+     * @return List of projects converted into project contract (JSON) type.
+     */
     @GetMapping(value = "/projects", produces = "application/json")
     public ResponseEntity<?> getAll() {
         try{
@@ -32,6 +37,11 @@ public class ProjectController {
         }
     }
 
+    /**
+     * This method will be invoked when API receives a GET request with a Project ID embedded in URL.
+     * @param id Project ID the user wants to retrieve
+     * @return a project contract (JSON) type of the project.
+     */
     @GetMapping(value = "/projects/{id}", produces = "application/json")
     public ResponseEntity<ProjectContract> getById(@PathVariable String id) {
         try {
@@ -43,6 +53,11 @@ public class ProjectController {
         }
     }
 
+    /**
+     * This method will be invoked when API receives a POST request with data of new project embedded in body - (JSON type)
+     * @param newProject data of new project
+     * @return a project contract (JSON) type of the newly created project.
+     */
     @PostMapping(value = "/projects", produces = "application/json")
     public ResponseEntity<ProjectContract> addNewProject(@RequestBody BaseProjectContract newProject) {
         try {
@@ -54,6 +69,11 @@ public class ProjectController {
         }
     }
 
+    /**
+     * This method will be invoked when API receives a DELETE request with a Project ID embedded in URL
+     * @param id Project ID the user wants to delete
+     * @return a project contract (JSON) type of the project.
+     */
     @DeleteMapping(value = "/projects/{id}", produces = "application/json")
     public ResponseEntity<?> removeProject(@PathVariable String id) {
         try{
@@ -68,6 +88,11 @@ public class ProjectController {
         }
     }
 
+    /**
+     * This method will be invoked when API receives a UPDATE request with a Project ID embedded in URL
+     * @param id Project ID the user wants to Update
+     * @return a project contract (JSON) type of the project.
+     */
     @PutMapping(value = "/projects/{id}", produces = "application/json")
     public ResponseEntity<?> updateProject(@RequestBody ProjectContract updatedProject, @PathVariable String id) {
         try {
@@ -78,5 +103,4 @@ public class ProjectController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
-
 }
