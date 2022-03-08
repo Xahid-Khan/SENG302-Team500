d893c3cae7e0cfc2a4d777a43b3dd22d2f421665
george <gca73@uclive.ac.nz>
2022-03-09 12:47:10 +1300

    Made the registerForm method which gets the registration form html page and the user data transfer object. Made the registerSubmit which maps the input values from the user object, it then returns a new html document with the user object details

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/RegistrationController.java
@@ -0,0 +1,32 @@
+package nz.ac.canterbury.seng302.portfolio.controller;
+
+import io.grpc.StatusRuntimeException;
+//import nz.ac.canterbury.seng302.portfolio.service.RegisterClientService;
+import nz.ac.canterbury.seng302.portfolio.DTO.UserTest;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRegisterResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import nz.ac.canterbury.seng302.portfolio.DTO.User;
+
+@Controller
+public class RegistrationController {
+
+    //
+    @GetMapping(value = "/register")//Mapped to GET
+    public String registerForm(Model model) { //model turns the view into a thymeleaf object
+        model.addAttribute("user", new User());//creates the DTO object which captures the inpuitd
+        return "registration_form"; //returns the view which renders the HTML content
+    }
+
+    //Method which revieves the User object from form. Can be referenced in done
+    @PostMapping("/register")//Mapped to post
+    public String registerSubmit(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);//reads post data to user object
+        System.out.println(user);
+        System.out.println(user.getEmail());
+
+        return "registered";
+    }
+}
