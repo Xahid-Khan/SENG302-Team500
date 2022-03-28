713eb4e4f98a8bacdd3dfce363bfc979fdff8452
Andrew Cook <aco155@uclive.ac.nz>
2022-03-29 02:19:24 +1300

    Added roles to the AuthenticateServerService so roles are added to the token.

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/AuthenticateServerService.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/AuthenticateServerService.java
@@ -2,8 +2,10 @@ package nz.ac.canterbury.seng302.identityprovider.service;
 
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.stream.Collectors;
 import net.devh.boot.grpc.server.service.GrpcService;
-
 import nz.ac.canterbury.seng302.identityprovider.authentication.AuthenticationServerInterceptor;
 import nz.ac.canterbury.seng302.identityprovider.authentication.JwtTokenUtil;
 import nz.ac.canterbury.seng302.identityprovider.database.UserModel;
@@ -12,67 +14,71 @@ import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthenticateRequest;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthenticateResponse;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthenticationServiceGrpc.AuthenticationServiceImplBase;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.util.List;
-
 @GrpcService
-public class AuthenticateServerService extends AuthenticationServiceImplBase{
+public class AuthenticateServerService extends AuthenticationServiceImplBase {
 
-    @Autowired
-    private UserRepository repository;
+  @Autowired private UserRepository repository;
 
-    @Autowired
-    private PasswordService passwordService;
+  @Autowired private PasswordService passwordService;
 
-    private final String ROLE_OF_USER = "student"; // Puce teams may want to change this to "teacher" to test some functionality
+  private JwtTokenUtil jwtTokenService = JwtTokenUtil.getInstance();
 
-    private JwtTokenUtil jwtTokenService = JwtTokenUtil.getInstance();
+  /** Attempts to authenticate a user with a given username and password. */
+  @Override
+  public void authenticate(
+      AuthenticateRequest request, StreamObserver<AuthenticateResponse> responseObserver) {
+    AuthenticateResponse.Builder reply = AuthenticateResponse.newBuilder();
 
-    /**
-     * Attempts to authenticate a user with a given username and password.
-     */
-    @Override
-    public void authenticate(AuthenticateRequest request, StreamObserver<AuthenticateResponse> responseObserver) {
-        AuthenticateResponse.Builder reply = AuthenticateResponse.newBuilder();
+    UserModel user = repository.findByUsername(request.getUsername());
+    try {
+      if (user != null
+          && passwordService.verifyPassword(request.getPassword(), user.getPasswordHash())) {
 
-        UserModel user = repository.findByUsername(request.getUsername());
-        try {
-            if (user != null && passwordService.verifyPassword(request.getPassword(), user.getPasswordHash())) {
+        String token =
+            jwtTokenService.generateTokenForUser(
+                user.getUsername(),
+                user.getId(),
+                user.getFirstName() + user.getLastName(),
+                // Encodes all roles a user has as a CSV
+                user.getRoles().stream()
+                    .map(UserRole::toString)
+                    .map(String::toLowerCase)
+                    .map(s -> s.replaceAll(" ", ""))
+                    .collect(Collectors.joining(",")));
 
-                String token = jwtTokenService.generateTokenForUser(user.getUsername(), user.getId(), user.getFirstName() + user.getLastName(), ROLE_OF_USER);
-                reply
-                        .setUserId(user.getId())
-                        .setUsername(user.getUsername())
-                        .setFirstName(user.getFirstName())
-                        .setLastName(user.getLastName())
-                        .setEmail(user.getEmail())
-                        .setMessage("Logged in successfully!")
-                        .setSuccess(true)
-                        .setToken(token);
-            } else {
-                reply
-                        .setMessage("Log in attempt failed: username or password incorrect")
-                        .setSuccess(false)
-                        .setToken("");
-            }
-            responseObserver.onNext(reply.build());
-            responseObserver.onCompleted();
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            e.printStackTrace();
-            responseObserver.onError(e);
-        }
+        reply
+            .setUserId(user.getId())
+            .setUsername(user.getUsername())
+            .setFirstName(user.getFirstName())
+            .setLastName(user.getLastName())
+            .setEmail(user.getEmail())
+            .setMessage("Logged in successfully!")
+            .setSuccess(true)
+            .setToken(token);
+      } else {
+        reply
+            .setMessage("Log in attempt failed: username or password incorrect")
+            .setSuccess(false)
+            .setToken("");
+      }
+      responseObserver.onNext(reply.build());
+      responseObserver.onCompleted();
+    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
     }
+  }
 
-    /**
-     * The AuthenticationInterceptor already handles validating the authState for us, so here we just need to
-     * retrieve that from the current context and return it in the gRPC body
-     */
-    @Override
-    public void checkAuthState(Empty request, StreamObserver<AuthState> responseObserver) {
-        responseObserver.onNext(AuthenticationServerInterceptor.AUTH_STATE.get());
-        responseObserver.onCompleted();
-    }
+  /**
+   * The AuthenticationInterceptor already handles validating the authState for us, so here we just
+   * need to retrieve that from the current context and return it in the gRPC body
+   */
+  @Override
+  public void checkAuthState(Empty request, StreamObserver<AuthState> responseObserver) {
+    responseObserver.onNext(AuthenticationServerInterceptor.AUTH_STATE.get());
+    responseObserver.onCompleted();
+  }
 }
