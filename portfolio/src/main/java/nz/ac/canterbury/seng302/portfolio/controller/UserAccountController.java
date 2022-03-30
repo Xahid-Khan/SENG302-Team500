a64655df02adf386e336b49b1fd6697bf1802e49
Cody Larsen <cal127@uclive.ac.nz>
2022-03-30 16:44:07 +1300

    Added roles to account page

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/UserAccountController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/UserAccountController.java
@@ -7,6 +7,7 @@ import nz.ac.canterbury.seng302.portfolio.service.UserAccountService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -45,6 +48,7 @@ public class UserAccountController {
         String registrationDate = "2 April 2021 (10 months)"; //TODO Sort this out
 
         //Prefill the form with the user's details
+        model.addAttribute("delegate", this);
         model.addAttribute("user", userDetails);
         model.addAttribute("registration_date", registrationDate);
 
@@ -80,6 +84,15 @@ public class UserAccountController {
                 userResponse.getPersonalPronouns(),
                 userResponse.getEmail(),
                 userResponse.getEmail());
+    }
+
+    public String formatUserRoles(List<UserRole> roles) {
+        return roles.stream().map(role -> switch (role) {
+            case STUDENT -> "Student";
+            case TEACHER -> "Teacher";
+            case COURSE_ADMINISTRATOR -> "Course Administrator";
+            default -> "Student";
+        }).collect(Collectors.joining(", "));
     }
 
 }
