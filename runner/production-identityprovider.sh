a5b5370375f19628919c8afc945e35eefb6265e0
Thomas Finlay <tfi18@uclive.ac.nz>
2022-05-10 20:47:44 +1200

    Added production properties and modified prod shell scripts to use them.

--- runner/production-identityprovider.sh
+++ runner/production-identityprovider.sh
@@ -1,3 +1,3 @@
 source runner_env.sh
 fuser -k 10500/tcp || true
-java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.application.name=identity-provider --grpc.server.port=10502 --server.port=10500
+java -jar production-identityprovider/libs/identityprovider-0.0.1-SNAPSHOT.jar --spring.config.additional-location=classpath:application.production.properties
