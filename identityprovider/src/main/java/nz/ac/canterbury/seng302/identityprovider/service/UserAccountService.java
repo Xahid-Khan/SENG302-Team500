371f3f217f9c14b277af60a2cd260335e3bc10a0
Andrew Cook <aco155@uclive.ac.nz>
2022-05-10 16:54:16 +1200

    Updated shared folder to include updated gRPC contracts.

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountService.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountService.java
@@ -35,8 +35,8 @@ public class UserAccountService extends UserAccountServiceGrpc.UserAccountServic
   @Autowired private ChangePasswordService changePasswordService;
 
   /**
-   * This is a GRPC user service method that is being over-ridden to register a user and return
-   * a UserRegisterRequest
+   * This is a GRPC user service method that is being over-ridden to register a user and return a
+   * UserRegisterRequest
    *
    * @param request parameters from the caller
    * @param responseObserver to receive results or errors
@@ -53,188 +53,189 @@ public class UserAccountService extends UserAccountServiceGrpc.UserAccountServic
       e.printStackTrace();
       responseObserver.onError(e);
     }
+  }
 
-    /**
-     * This is a GRPC user service method that is being over-ridden to edit the users details and return
-     *    * a EditUserResponse
-     *
-     * @param request parameters from the caller
-     * @param responseObserver to receive results or errors
-     */
-    @Override
-    public void editUser(EditUserRequest request, StreamObserver<EditUserResponse> responseObserver) {
-        try {
-            var response = editUserService.editUser(request);
+  /**
+   * This is a GRPC user service method that is being over-ridden to edit the users details and
+   * return * a EditUserResponse
+   *
+   * @param request parameters from the caller
+   * @param responseObserver to receive results or errors
+   */
+  @Override
+  public void editUser(EditUserRequest request, StreamObserver<EditUserResponse> responseObserver) {
+    try {
+      var response = editUserService.editUser(request);
 
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            e.printStackTrace();
-            responseObserver.onError(e);
-        }
-    }
-
-    /**
-     * This is a GRPC user service method that is being over-ridden to get the user details and encase
-     * them into a User Response body. if the user is not found the User response is set to null
-     *
-     * @param request parameters from the caller
-     * @param responseObserver to receive results or errors
-     */
-    @Override
-    public void getUserAccountById(GetUserByIdRequest request, StreamObserver<UserResponse> responseObserver) {
-        try {
-            var res = getUserService.getUserAccountById(request);
-
-            responseObserver.onNext(res);
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            e.printStackTrace();
-            responseObserver.onError(e);
-        }
-    }
-
-    /**
-     * GRPC service method that provides a list of user details with a caller-supplied sort order,
-     * maximum length, and offset.
-     *
-     * @param request parameters from the caller
-     * @param responseObserver to receive results or errors
-     */
-    @Override
-    public void getPaginatedUsers(
-            GetPaginatedUsersRequest request, StreamObserver<PaginatedUsersResponse> responseObserver) {
-        try {
-            var response = getUserService.getPaginatedUsers(request);
-
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            responseObserver.onError(e);
-        }
-    }
-
-    /**
-     * Adds a role to the user if the user does not already have the role.
-     *
-     * @param modificationRequest the modification request for the role
-     * @param responseObserver to receive results or errors
-     */
-    @Override
-    public void addRoleToUser(
-            ModifyRoleOfUserRequest modificationRequest,
-            StreamObserver<UserRoleChangeResponse> responseObserver) {
-        try {
-            var response = roleService.addRoleToUser(modificationRequest);
-
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        } catch (UserDoesNotExistException e) {
-            e.printStackTrace();
-            responseObserver.onError(e);
-        }
-    }
-
-    /**
-     * Removes a role to the user if the user does not already have the role.
-     *
-     * @param modificationRequest The modification request for the role
-     * @param responseObserver to receive results or errors
-     */
-    @Override
-    public void removeRoleFromUser(
-            ModifyRoleOfUserRequest modificationRequest,
-            StreamObserver<UserRoleChangeResponse> responseObserver) {
-        try {
-            var response = roleService.removeRoleFromUser(modificationRequest);
-
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        } catch (UserDoesNotExistException | IrremovableRoleException e) {
-            e.printStackTrace();
-            responseObserver.onError(e);
-        }
-    }
-
-
-    /**
-     * Uploads the stream of data from User and sends back the status response based on the upload status.
-     *
-     * @param responseObserver is of FileUploadStatusResponse
-     * @return a UploadUserProfilePhotoRequest
-     */
-    @Override
-    public StreamObserver<UploadUserProfilePhotoRequest> uploadUserProfilePhoto(StreamObserver<FileUploadStatusResponse> responseObserver) {
-        return new StreamObserver<UploadUserProfilePhotoRequest>() {
-            Integer userId = null;
-            String fileType = null;
-            final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-            @Override
-            public void onNext(UploadUserProfilePhotoRequest request) {
-                FileUploadStatusResponse.Builder reply = FileUploadStatusResponse.newBuilder();
-
-                if (request.hasMetaData()) {
-                    userId = request.getMetaData().getUserId();
-                    fileType = request.getMetaData().getFileType();
-
-                    reply.setStatus(FileUploadStatus.PENDING);
-                    reply.setMessage("Pending");
-                }
-                else {
-                    if (userId == null) {
-                        responseObserver.onError(new Throwable("Image chunk must be sent after metadata."));
-                        return;
-                    }
-
-                    ByteString chunk = request.getFileContent();
-                    try {
-                        buffer.write(chunk.toByteArray());
-                    }
-                    catch (IOException e) {
-                        responseObserver.onError(new Throwable("Exception occurred while loading image chunk."));
-                        return;
-                    }
-
-                    reply.setStatus(FileUploadStatus.IN_PROGRESS);
-                    reply.setMessage("Chunk received.");
-                }
-
-                responseObserver.onNext(reply.build());
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                System.out.println(t.getMessage());
-            }
-
-            @Override
-            public void onCompleted() {
-                if (buffer.size() == 0) {
-                    responseObserver.onError(new Throwable("Image data must be transferred."));
-                    return;
-                }
-
-                FileUploadStatusResponse uploadStatus = editUserService.UploadUserPhoto(userId, buffer.toByteArray());
-
-                if (uploadStatus.getStatus() == FileUploadStatus.FAILED) {
-                    responseObserver.onError(new Throwable(String.valueOf(uploadStatus.getStatus())));
-                    return;
-                }
-
-                FileUploadStatusResponse.Builder reply = FileUploadStatusResponse.newBuilder();
-                reply.setStatus(uploadStatus.getStatus())
-                        .setMessage(uploadStatus.getMessage());
-                responseObserver.onNext(reply.build());
-            }
-        };
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (Exception e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
     }
   }
 
+  /**
+   * This is a GRPC user service method that is being over-ridden to get the user details and encase
+   * them into a User Response body. if the user is not found the User response is set to null
+   *
+   * @param request parameters from the caller
+   * @param responseObserver to receive results or errors
+   */
   @Override
-  public void changeUserPassword(ChangePasswordRequest request, StreamObserver<ChangePasswordResponse> responseObserver) {
+  public void getUserAccountById(
+      GetUserByIdRequest request, StreamObserver<UserResponse> responseObserver) {
+    try {
+      var res = getUserService.getUserAccountById(request);
+
+      responseObserver.onNext(res);
+      responseObserver.onCompleted();
+    } catch (Exception e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
+    }
+  }
+
+  /**
+   * GRPC service method that provides a list of user details with a caller-supplied sort order,
+   * maximum length, and offset.
+   *
+   * @param request parameters from the caller
+   * @param responseObserver to receive results or errors
+   */
+  @Override
+  public void getPaginatedUsers(
+      GetPaginatedUsersRequest request, StreamObserver<PaginatedUsersResponse> responseObserver) {
+    try {
+      var response = getUserService.getPaginatedUsers(request);
+
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (Exception e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
+    }
+  }
+
+  /**
+   * Adds a role to the user if the user does not already have the role.
+   *
+   * @param modificationRequest the modification request for the role
+   * @param responseObserver to receive results or errors
+   */
+  @Override
+  public void addRoleToUser(
+      ModifyRoleOfUserRequest modificationRequest,
+      StreamObserver<UserRoleChangeResponse> responseObserver) {
+    try {
+      var response = roleService.addRoleToUser(modificationRequest);
+
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (UserDoesNotExistException e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
+    }
+  }
+
+  /**
+   * Removes a role to the user if the user does not already have the role.
+   *
+   * @param modificationRequest The modification request for the role
+   * @param responseObserver to receive results or errors
+   */
+  @Override
+  public void removeRoleFromUser(
+      ModifyRoleOfUserRequest modificationRequest,
+      StreamObserver<UserRoleChangeResponse> responseObserver) {
+    try {
+      var response = roleService.removeRoleFromUser(modificationRequest);
+
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (UserDoesNotExistException | IrremovableRoleException e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
+    }
+  }
+
+  /**
+   * Uploads the stream of data from User and sends back the status response based on the upload
+   * status.
+   *
+   * @param responseObserver is of FileUploadStatusResponse
+   * @return a UploadUserProfilePhotoRequest
+   */
+  @Override
+  public StreamObserver<UploadUserProfilePhotoRequest> uploadUserProfilePhoto(
+      StreamObserver<FileUploadStatusResponse> responseObserver) {
+    return new StreamObserver<UploadUserProfilePhotoRequest>() {
+      Integer userId = null;
+      String fileType = null;
+      final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+      @Override
+      public void onNext(UploadUserProfilePhotoRequest request) {
+        FileUploadStatusResponse.Builder reply = FileUploadStatusResponse.newBuilder();
+
+        if (request.hasMetaData()) {
+          userId = request.getMetaData().getUserId();
+          fileType = request.getMetaData().getFileType();
+
+          reply.setStatus(FileUploadStatus.PENDING);
+          reply.setMessage("Pending");
+        } else {
+          if (userId == null) {
+            responseObserver.onError(new Throwable("Image chunk must be sent after metadata."));
+            return;
+          }
+
+          ByteString chunk = request.getFileContent();
+          try {
+            buffer.write(chunk.toByteArray());
+          } catch (IOException e) {
+            responseObserver.onError(
+                new Throwable("Exception occurred while loading image chunk."));
+            return;
+          }
+
+          reply.setStatus(FileUploadStatus.IN_PROGRESS);
+          reply.setMessage("Chunk received.");
+        }
+
+        responseObserver.onNext(reply.build());
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        System.out.println(t.getMessage());
+      }
+
+      @Override
+      public void onCompleted() {
+        if (buffer.size() == 0) {
+          responseObserver.onError(new Throwable("Image data must be transferred."));
+          return;
+        }
+
+        FileUploadStatusResponse uploadStatus =
+            editUserService.UploadUserPhoto(userId, buffer.toByteArray());
+
+        if (uploadStatus.getStatus() == FileUploadStatus.FAILED) {
+          responseObserver.onError(new Throwable(String.valueOf(uploadStatus.getStatus())));
+          return;
+        }
+
+        FileUploadStatusResponse.Builder reply = FileUploadStatusResponse.newBuilder();
+        reply.setStatus(uploadStatus.getStatus()).setMessage(uploadStatus.getMessage());
+        responseObserver.onNext(reply.build());
+      }
+    };
+  }
+
+  @Override
+  public void changeUserPassword(
+      ChangePasswordRequest request, StreamObserver<ChangePasswordResponse> responseObserver) {
     try {
       var response = changePasswordService.changePassword(request);
 
@@ -242,8 +243,8 @@ public class UserAccountService extends UserAccountServiceGrpc.UserAccountServic
       responseObserver.onCompleted();
 
     } catch (UserDoesNotExistException e) {
-        e.printStackTrace();
-        responseObserver.onError(e);
+      e.printStackTrace();
+      responseObserver.onError(e);
     } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
       e.printStackTrace();
     }
