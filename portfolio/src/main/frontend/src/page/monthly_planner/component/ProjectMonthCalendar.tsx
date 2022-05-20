da7d72397a7f5fccc4a1e9c1455279bc39c5aed4
Andrew Cook <aco155@uclive.ac.nz>
2022-05-20 14:47:32 +1200

    Brought out getContrast into a new Util file.

--- portfolio/src/main/frontend/src/page/monthly_planner/component/ProjectMonthCalendar.tsx
+++ portfolio/src/main/frontend/src/page/monthly_planner/component/ProjectMonthCalendar.tsx
@@ -8,7 +8,7 @@ import {ToastBase} from "../../../component/toast/ToastBase";
 import defaultToastTheme from "../../../component/toast/DefaultToast.module.css";
 import {LoadingErrorPresenter} from "../../../component/error/LoadingErrorPresenter";
 import {DatetimeUtils} from "../../../util/DatetimeUtils";
-import {LoadingPending} from "../../../util/network/loading_status";
+import {getContrast} from "../../../util/TextColorUtil";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
@@ -92,35 +92,3 @@ export const ProjectMonthCalendar: React.FC = observer(() => {
         </>
     )
 })
-
-/**
- * Get the contrasting color for any hex color
- * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
- * Derived from work by Brian Suda, https://24ways.org/2010/calculating-color-contrast/
- * @param  {String} A hexcolor value
- * @return {String} The contrasting color (black or white)
- */
-function getContrast (hexcolor: string): string{
-    // If a leading # is provided, remove it
-    if (hexcolor.slice(0, 1) === '#') {
-        hexcolor = hexcolor.slice(1);
-    }
-
-    // If a three-character hexcode, make six-character
-    if (hexcolor.length === 3) {
-        hexcolor = hexcolor.split('').map(function (hex) {
-            return hex + hex;
-        }).join('');
-    }
-
-    // Convert to RGB value
-    let r = parseInt(hexcolor.substring(0,2),16);
-    let g = parseInt(hexcolor.substring(2,4),16);
-    let b = parseInt(hexcolor.substring(4,6),16);
-
-    // Get YIQ ratio
-    let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-
-    // Check contrast
-    return (yiq >= 128) ? 'black' : 'white';
-}
