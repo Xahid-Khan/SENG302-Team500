ccf65610e6303e02cb204b3b119e64026b8f3787
Thomas Finlay <tfi18@uclive.ac.nz>
2022-06-06 17:51:39 +1200

    Resolved CheckStyle issues

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/LiveProjectUpdatesController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/LiveProjectUpdatesController.java
@@ -1,26 +1,31 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.authentication.PortfolioPrincipal;
-import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 /**
- * Controller for handling and dispatching live project update notifications
+ * Controller for handling and dispatching live project update notifications.
  */
 @Controller
 public class LiveProjectUpdatesController {
 
-    @MessageMapping("/ping")
-    @SendTo("/topic/pongs")
-    public String ping(@AuthenticationPrincipal PreAuthenticatedAuthenticationToken principal, String message) throws Exception {
-        var authState = (PortfolioPrincipal) principal.getPrincipal();
-        return String.format("Pong %s (Also, hi %s!)", message, authState.getName());
-    }
+  /**
+   * STOMP endpoint that receives a message string and echos it back with metadata attached.
+   *
+   * <p>
+   *   This can be used in conjunction with the socket_test page to verify that the WebSocket setup
+   *   is working as expected.
+   * </p>
+   */
+  @MessageMapping("/ping")
+  @SendTo("/topic/pongs")
+  public String ping(@AuthenticationPrincipal PreAuthenticatedAuthenticationToken principal,
+      String message) throws Exception {
+    var authState = (PortfolioPrincipal) principal.getPrincipal();
+    return String.format("Pong %s (Also, hi %s!)", message, authState.getName());
+  }
 }
