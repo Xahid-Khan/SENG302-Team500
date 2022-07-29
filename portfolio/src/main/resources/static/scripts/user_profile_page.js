dcc3bb269283e69f9a73d2e8a7ccbab56393f37e
Zahid <zkh22@uclive.ac.nz>
2022-07-29 22:40:17 +1200

    Restyled the user profile photo. Disabled the button when there is no photo. Worked with shaylin on confirmation modal for DeletePhoto button.

---
+++ portfolio/src/main/resources/static/scripts/user_profile_page.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const userImage = document.getElementById("userPhoto").getAttribute("src")
+
+
+async function activateDeletePhotoButton() {
+    console.log(userImage)
+    if (userImage === "https://humanimals.co.nz/wp-content/uploads/2019/11/blank-profile-picture-973460_640.png") {
+        document.getElementById("deleteUserPhoto").setAttribute("disabled", "disabled")
+    } else {
+        document.getElementById("deleteUserPhoto").setAttribute("disabled", "")
+    }
+}
+
+document.getElementById("userPhoto").addEventListener("load", activateDeletePhotoButton)
+
+const modalDeleteContainer = document.getElementById(`modal-delete-open`);
+const modalDeleteX = document.getElementById(`modal-delete-x`);
+const modalDeleteCancel = document.getElementById(`modal-delete-cancel`);
+const modalDeleteConfirm = document.getElementById(`modal-delete-confirm`);
+
+const openDeleteModal = () => {
+    modalDeleteContainer.style.display='block';
+    modalDeleteX.addEventListener("click",()=>cancelDeleteModal())
+    modalDeleteCancel.addEventListener("click",()=>cancelDeleteModal())
+    modalDeleteConfirm.addEventListener("click",()=>confirmDeleteModal())
+}
+
+const cancelDeleteModal = () => {
+    modalDeleteContainer.style.display='none';
+    modalDeleteX.removeEventListener("click",()=>cancelDeleteModal())
+    modalDeleteCancel.removeEventListener("click",()=>cancelDeleteModal())
+    modalDeleteConfirm.removeEventListener("click",()=>confirmDeleteModal())
+
+}
+
+const confirmDeleteModal = () => {
+    modalDeleteContainer.style.display='none';
+    modalDeleteX.removeEventListener("click",()=>cancelDeleteModal())
+    modalDeleteCancel.removeEventListener("click",()=>cancelDeleteModal())
+    modalDeleteConfirm.removeEventListener("click",()=>confirmDeleteModal())
+    document.getElementById("deletePhotoSubmissionForm").click();
+}
+
+document.getElementById("deletePhotoForm").addEventListener('click', ()=> openDeleteModal())
