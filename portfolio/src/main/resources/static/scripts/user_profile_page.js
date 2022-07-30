18d95a36857c6ae35d5b95a9cbff4a448c682484
Zahid <zkh22@uclive.ac.nz>
2022-07-30 12:45:17 +1200

    fixing delete photo button for VM.

--- portfolio/src/main/resources/static/scripts/user_profile_page.js
+++ portfolio/src/main/resources/static/scripts/user_profile_page.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const userImage = document.getElementById("userPhoto").getAttribute("src")
+const userImage = document.getElementById("userPhoto").getAttribute("src");
 
 
 async function activateDeletePhotoButton() {
-    console.log("I'm Here....")
-    console.log(userImage)
-    console.log("https://humanimals.co.nz/wp-content/uploads/2019/11/blank-profile-picture-973460_640.png")
+    console.log("I'm Here....");
+    console.log(userImage);
+    console.log("https://humanimals.co.nz/wp-content/uploads/2019/11/blank-profile-picture-973460_640.png");
     if (userImage === "https://humanimals.co.nz/wp-content/uploads/2019/11/blank-profile-picture-973460_640.png") {
-        document.getElementById("deleteUserPhoto").setAttribute("disabled", "true")
+        document.getElementById("deleteUserPhoto").setAttribute("disabled", "true");
     } else {
-        document.getElementById("deleteUserPhoto").removeAttribute("disabled")
+        document.getElementById("deleteUserPhoto").removeAttribute("disabled");
     }
 }
-// window.addEventListener('load', activateDeletePhotoButton)
-// window.onload = () => activateDeletePhotoButton()
-document.getElementById("userPhoto").onload(activateDeletePhotoButton)
+window.addEventListener('load', activateDeletePhotoButton);
 
 const modalDeleteContainer = document.getElementById(`modal-delete-open`);
 const modalDeleteX = document.getElementById(`modal-delete-x`);
@@ -24,25 +22,25 @@ const modalDeleteConfirm = document.getElementById(`modal-delete-confirm`);
 
 const openDeleteModal = () => {
     modalDeleteContainer.style.display='block';
-    modalDeleteX.addEventListener("click",()=>cancelDeleteModal())
-    modalDeleteCancel.addEventListener("click",()=>cancelDeleteModal())
-    modalDeleteConfirm.addEventListener("click",()=>confirmDeleteModal())
+    modalDeleteX.addEventListener("click",()=>cancelDeleteModal());
+    modalDeleteCancel.addEventListener("click",()=>cancelDeleteModal());
+    modalDeleteConfirm.addEventListener("click",()=>confirmDeleteModal());
 }
 
 const cancelDeleteModal = () => {
     modalDeleteContainer.style.display='none';
-    modalDeleteX.removeEventListener("click",()=>cancelDeleteModal())
-    modalDeleteCancel.removeEventListener("click",()=>cancelDeleteModal())
-    modalDeleteConfirm.removeEventListener("click",()=>confirmDeleteModal())
+    modalDeleteX.removeEventListener("click",()=>cancelDeleteModal());
+    modalDeleteCancel.removeEventListener("click",()=>cancelDeleteModal());
+    modalDeleteConfirm.removeEventListener("click",()=>confirmDeleteModal());
 
 }
 
 const confirmDeleteModal = () => {
     modalDeleteContainer.style.display='none';
-    modalDeleteX.removeEventListener("click",()=>cancelDeleteModal())
-    modalDeleteCancel.removeEventListener("click",()=>cancelDeleteModal())
-    modalDeleteConfirm.removeEventListener("click",()=>confirmDeleteModal())
+    modalDeleteX.removeEventListener("click",()=>cancelDeleteModal());
+    modalDeleteCancel.removeEventListener("click",()=>cancelDeleteModal());
+    modalDeleteConfirm.removeEventListener("click",()=>confirmDeleteModal());
     document.getElementById("deletePhotoSubmissionForm").click();
 }
 
-document.getElementById("deleteUserPhoto").addEventListener('click', ()=> openDeleteModal())
+document.getElementById("deleteUserPhoto").addEventListener('click', ()=> openDeleteModal());
