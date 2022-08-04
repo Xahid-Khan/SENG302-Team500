93611e00b154559cc34b953d1f6167e9a96468a6
Andrew Cook <aco155@uclive.ac.nz>
2022-08-04 17:49:38 +1200

    GroupRepository and GroupModel cleanup

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/database/GroupModel.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/database/GroupModel.java
@@ -1,34 +1,41 @@
 package nz.ac.canterbury.seng302.identityprovider.database;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/**
+ * Handles having groups as models for the database.
+ */
 @Entity
 public class GroupModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
 
-    @Column(unique = true, nullable = false)
-    private String shortName;
+  @Column(unique = true, nullable = false)
+  private String shortName;
 
-    private String longName;
+  private String longName;
 
-    protected GroupModel () {}
+  protected GroupModel() {}
 
-    public GroupModel (String shortName, String longName){
-        this.shortName = shortName;
-        this.longName = longName;
-    }
+  public GroupModel(String shortName, String longName) {
+    this.shortName = shortName;
+    this.longName = longName;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public String getShortName() {
-        return shortName;
-    }
+  public String getShortName() {
+    return shortName;
+  }
 
-    public String getLongName() {
-        return longName;
-    }
+  public String getLongName() {
+    return longName;
+  }
 }
