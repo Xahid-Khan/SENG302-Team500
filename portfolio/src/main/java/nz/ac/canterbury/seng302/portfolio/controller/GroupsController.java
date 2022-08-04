949c9e4d6ddcc8ce9df56e3eae42eb63774e93fd
Andrew Cook <aco155@uclive.ac.nz>
2022-08-04 19:31:46 +1200

    Made the header a common fragment. Updated HTML.
    Also fixed a bug in which the user would lose their username upon an invalid submission to editing their account details.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupsController.java
@@ -13,35 +13,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-/**
- * Handles the GET request on the /groups endpoint
- */
+/** Handles the GET request on the /groups endpoint */
 @Controller
 public class GroupsController {
-    @Autowired
-    private UserAccountService userAccountService;
+  @Autowired private UserAccountService userAccountService;
 
-    @Autowired
-    private AuthStateService authStateService;
+  @Autowired private AuthStateService authStateService;
 
-    /**
-     * GET /groups fetches the groups page. The groups page shows all groups
-     *
-     * @param principal Principal for the currently logged in user, used to get userId
-     * @param model Parameters sent to thymeleaf template to be rendered into HTML
-     * @return The groups html page
-     */
-    @GetMapping(value="/groups")
-    public String getGroups(@AuthenticationPrincipal PortfolioPrincipal principal, Model model) {
-        Integer userId = authStateService.getId(principal);
+  /**
+   * GET /groups fetches the groups page. The groups page shows all groups
+   *
+   * @param principal Principal for the currently logged in user, used to get userId
+   * @param model Parameters sent to thymeleaf template to be rendered into HTML
+   * @return The groups html page
+   */
+  @GetMapping(value = "/groups")
+  public String getGroups(@AuthenticationPrincipal PortfolioPrincipal principal, Model model) {
+    Integer userId = authStateService.getId(principal);
 
-        UserResponse userDetails = userAccountService.getUserById(userId);
-        List<UserRole> roles = userDetails.getRolesList();
+    UserResponse userDetails = userAccountService.getUserById(userId);
+    List<UserRole> roles = userDetails.getRolesList();
 
-        model.addAttribute("userId", userId);
-        model.addAttribute("canEdit", roles.contains(UserRole.TEACHER) || roles.contains(UserRole.COURSE_ADMINISTRATOR));
-        model.addAttribute("username", userDetails.getUsername());
+    model.addAttribute("userId", userId);
+    model.addAttribute(
+        "canEdit",
+        roles.contains(UserRole.TEACHER) || roles.contains(UserRole.COURSE_ADMINISTRATOR));
+    model.addAttribute("username", userDetails.getUsername());
 
-        return "groups";
-    }
+    return "groups";
+  }
 }
