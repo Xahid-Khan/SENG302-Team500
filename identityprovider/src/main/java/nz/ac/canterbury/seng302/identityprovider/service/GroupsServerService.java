7311fac9840b08d7f0a1e7ebb22fee5c4c68f586
Andrew Cook <aco155@uclive.ac.nz>
2022-08-04 18:29:49 +1200

    Added deleting functionality on client and server side

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/GroupsServerService.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/GroupsServerService.java
@@ -6,24 +6,24 @@ import nz.ac.canterbury.seng302.identityprovider.database.GroupModel;
 import nz.ac.canterbury.seng302.identityprovider.database.GroupRepository;
 import nz.ac.canterbury.seng302.shared.identityprovider.CreateGroupRequest;
 import nz.ac.canterbury.seng302.shared.identityprovider.CreateGroupResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.DeleteGroupRequest;
+import nz.ac.canterbury.seng302.shared.identityprovider.DeleteGroupResponse;
 import nz.ac.canterbury.seng302.shared.util.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * This service handles all requests on the server side to manage groups.
- */
+/** This service handles all requests on the server side to manage groups. */
 @Service
 public class GroupsServerService {
   @Autowired private GroupRepository groupRepository;
 
   /**
    * Handles the functionality on the server side for creating groups. It will ensure that both the
-   *  short name and the long name of the group are unique. If they are, it will add the group to
-   *  the database. Otherwise, it will fail and return with validation errors.
+   * short name and the long name of the group are unique. If they are, it will add the group to the
+   * database. Otherwise, it will fail and return with validation errors.
    *
-   * @param groupRequest  the CreateGroupRequest gRPC message
-   * @return              a CreateGroupResponse gRPC message with potential errors
+   * @param groupRequest the CreateGroupRequest gRPC message
+   * @return a CreateGroupResponse gRPC message with potential errors
    */
   public CreateGroupResponse createGroup(CreateGroupRequest groupRequest) {
     List<ValidationError> validationErrors = new ArrayList<>();
@@ -60,6 +60,29 @@ public class GroupsServerService {
           .setNewGroupId(-1)
           .setMessage("Validation error(s)")
           .addAllValidationErrors(validationErrors)
+          .build();
+    }
+  }
+
+  /**
+   * Handles the functionality on the server side for deleting groups. If the database does not have
+   * a group with the corresponding ID, then a failure will be returned. Otherwise, the group will
+   * be deleted, and a success will be returned.
+   *
+   * @param groupRequest the DeleteGroupRequest gRPC message
+   * @return a DeleteGroupResponse gRPC message
+   */
+  public DeleteGroupResponse deleteGroup(DeleteGroupRequest groupRequest) {
+    if (groupRepository.findById(groupRequest.getGroupId()).isEmpty()) {
+      return DeleteGroupResponse.newBuilder()
+          .setIsSuccess(false)
+          .setMessage("Error: Group does not exist")
+          .build();
+    } else {
+      groupRepository.deleteById(groupRequest.getGroupId());
+      return DeleteGroupResponse.newBuilder()
+          .setIsSuccess(true)
+          .setMessage("Group successfully deleted")
           .build();
     }
   }
