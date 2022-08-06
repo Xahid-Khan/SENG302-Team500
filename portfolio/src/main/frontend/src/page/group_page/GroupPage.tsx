054a7f45b07fae81f4184acd90c85819e6c63d54
George Carr-Smith <gca73@uclive.ac.nz>
2022-08-06 13:46:14 +1200

    Fixed the documentation #fix

--- portfolio/src/main/frontend/src/page/group_page/GroupPage.tsx
+++ portfolio/src/main/frontend/src/page/group_page/GroupPage.tsx
@@ -4,7 +4,7 @@ import {ToasterRoot} from "../../component/toast/ToasterRoot";
 import {ShowAllGroups} from "./ShowAllGroups";
 
 /**
- * The root of the MonthlyPlannerPage. This does a few jobs:
+ * The root of the GroupPage. This does a few jobs:
  * 1. Construct the GroupPage and wrap the whole page in a Provider so it can be used by children.
  * 2. Wrap the entire page in a PageLayout.
  * 3. Place the PageContent component inside that Layout component.
