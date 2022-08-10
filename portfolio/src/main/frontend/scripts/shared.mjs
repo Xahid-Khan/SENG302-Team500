021e51974a3484e48312f56870931ca9111a884b
Zahid <zkh22@uclive.ac.nz>
2022-08-10 15:31:19 +1200

    added some documentation and a check to check for minimum file size, and performed manual tests.

--- portfolio/src/main/frontend/scripts/shared.mjs
+++ portfolio/src/main/frontend/scripts/shared.mjs
@@ -4,6 +4,6 @@ export const entryPointPaths = [
   "./src/entry/monthly_planner.tsx",
   "./src/entry/project_details.js",
   "./src/entry/groups.tsx",
-  "./src/entry/live_updating.tsx",
+  "./src/entry/live_updating.ts",
   "./src/entry/cropping_image.tsx"
 ]
