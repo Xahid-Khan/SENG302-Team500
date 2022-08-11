9eadca1160e5da1a0a3a64d4eb67e55b08bef70e
Cody Larsen <cal127@uclive.ac.nz>
2022-08-11 21:53:23 +1200

    Tried to revert some changes to see if that works

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/AddingBaseGroups.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/AddingBaseGroups.java
@@ -1,20 +1,23 @@
 package nz.ac.canterbury.seng302.identityprovider.service;
 
-import nz.ac.canterbury.seng302.identityprovider.database.GroupMemberModel;
-import nz.ac.canterbury.seng302.identityprovider.database.GroupMemberRepository;
-import nz.ac.canterbury.seng302.identityprovider.database.GroupModel;
-import nz.ac.canterbury.seng302.identityprovider.database.GroupRepository;
+import com.google.protobuf.Timestamp;
+import nz.ac.canterbury.seng302.identityprovider.database.*;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class AddingBaseGroups {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private GroupRepository groupRepository;
@@ -23,12 +26,76 @@ public class AddingBaseGroups {
     @Autowired
     private GroupMemberRepository groupMemberRepository;
 
+    @Autowired
+    private PasswordService passwordService;
+
+    private GroupModel teachers;
+    private GroupModel nonGroup;
+
     /**
-     * This method is listing to the application ready event, after application is ready it calls a function creates 2 groups
+     * This method is listingint to the application ready event, after application is ready it creates 2 student users and
+     * 1 teacher user, then creates 1 teaching staff group (group of users who are teacher) and 1 non-group (group for users who are not teacher).
+     * Then it adds those users into their respect groups.
+     * @throws NoSuchAlgorithmException this exception is thrown by password service
+     * @throws InvalidKeySpecException this exception is thrown by password service
      */
     @EventListener({ApplicationReadyEvent.class})
-    public void addUsersToDatabase() {
-        createTeacherAndNonGroup();
+    public void addUsersToDatabase() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        if (userRepository.findByUsername("student") == null) {
+            var passwordHash = passwordService.hashPassword("123456789");
+            List<UserRole> userRole = new ArrayList<>();
+            userRole.add(UserRole.STUDENT);
+            String pronouns = "His/Him";
+            UserModel student =
+                    new UserModel(
+                            "student",
+                            passwordHash,
+                            "Default",
+                            "",
+                            "Student",
+                            "",
+                            "I am a new Student",
+                            pronouns,
+                            "student@default.com",
+                            userRole,
+                            currentTimestamp());
+
+            userRepository.save(student);
+            userRole.add(UserRole.TEACHER);
+
+            UserModel teacher =
+                    new UserModel(
+                            "teacher",
+                            passwordHash,
+                            "Default",
+                            "",
+                            "Teacher",
+                            "",
+                            "I am a new Teacher",
+                            pronouns,
+                            "teacher@default.com",
+                            userRole,
+                            currentTimestamp());
+            userRepository.save(teacher);
+
+            UserModel admin =
+                    new UserModel(
+                            "admin",
+                            passwordHash,
+                            "Default",
+                            "",
+                            "Admin",
+                            "",
+                            "I am a new Admin",
+                            pronouns,
+                            "admin@default.com",
+                            userRole,
+                            currentTimestamp());
+            userRepository.save(admin);
+
+            createTeacherAndNonGroup();
+            addTeachersAndNonGroupMembers();
+        }
     }
 
 
@@ -36,16 +103,41 @@ public class AddingBaseGroups {
      * This method creates 2 groups.
      */
     public void createTeacherAndNonGroup() {
-//        if (groupRepository.findByShortName("Teachers") == null) {
-//            GroupModel teachers = new GroupModel("Teachers", "Teaching Team");
-//            GroupModel nonGroup = new GroupModel("Non Group", "Users without a group");
-//            groupRepository.save(teachers);
-//            groupRepository.save(nonGroup);
-//            GroupMemberModel teacherGroupMembers = new GroupMemberModel(groupRepository.findByShortName("Teachers").getId(), List.of());
-//            groupMemberRepository.save(teacherGroupMembers);
-//            GroupMemberModel nonGroupMembers = new GroupMemberModel(groupRepository.findByShortName("Non Group").getId(), List.of());
-//            groupMemberRepository.save(nonGroupMembers);
-//        }
+        teachers = new GroupModel("Teachers", "Teaching Team");
+        nonGroup = new GroupModel("Non Group", "Users without a group");
+        groupRepository.save(teachers);
+        groupRepository.save(nonGroup);
+
     }
 
+    /**
+     * This method adds users to the group based on their roles.
+     */
+    public void addTeachersAndNonGroupMembers() {
+        List<Integer> areTeachers = new ArrayList<>();
+        List<Integer> allOtherUsers = new ArrayList<>();
+
+        Iterable<UserModel> allUsers = userRepository.findAll();
+        allUsers.forEach(userModel -> {
+            if(userModel.getRoles().contains(UserRole.TEACHER)) {
+                areTeachers.add(userModel.getId());
+            } else {
+                allOtherUsers.add(userModel.getId());
+            }
+        });
+
+        GroupMemberModel allTeachers = new GroupMemberModel(teachers.getId(), areTeachers);
+        groupMemberRepository.save(allTeachers);
+        GroupMemberModel notTeachers = new GroupMemberModel(nonGroup.getId(), allOtherUsers);
+        groupMemberRepository.save(notTeachers);
+    }
+
+    /**
+     * Helper function to get the current timestamp.
+     *
+     * @return the current timestamp
+     */
+    private static Timestamp currentTimestamp() {
+        return Timestamp.newBuilder().setSeconds(Instant.now().getEpochSecond()).build();
+    }
 }
