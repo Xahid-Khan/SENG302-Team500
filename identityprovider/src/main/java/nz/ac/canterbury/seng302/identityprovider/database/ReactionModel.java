22a79eeb8cbc50e9b36d93115947edf808558d8b
Shaylin Simadari <ssi162@uclive.ac.nz>
2022-08-20 20:51:09 +1200

    Added an id value to ReactionModel as the composite key was causing an error. #fix

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/database/ReactionModel.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/database/ReactionModel.java
@@ -8,14 +8,23 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "reaction_model")
 public class ReactionModel {
-
     @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     @Column(name = "user_id", nullable = false)
     private int userId;
 
-    @Id
     @Column(name = "post_id", nullable = false)
     private int postId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public int getUserId() {
         return userId;
