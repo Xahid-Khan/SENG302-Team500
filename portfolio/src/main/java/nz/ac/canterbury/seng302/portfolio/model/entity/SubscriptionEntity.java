29af07651ea6784fa4e11cde3ed09c9e45eced70
Shaylin Simadari <ssi162@uclive.ac.nz>
2022-08-24 00:54:18 +1200

    Created a subscription entity and repository to store user to group subscriptions in the database.
    - The subscription also stores a timestamp of when the user subscribed. This could be useful for showing all posts since a user subscribed.

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/entity/SubscriptionEntity.java
@@ -0,0 +1,60 @@
+package nz.ac.canterbury.seng302.portfolio.model.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * Represents a subscription of a user to a group in the database
+ */
+@Entity
+@Table(name = "subscription")
+public class SubscriptionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "group_id", nullable = false)
+    private int groupId;
+
+    @CreationTimestamp
+    @Column(name = "time_subscribed")
+    private Timestamp timeSubscribed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public Timestamp getTimeSubscribed() {
+        return timeSubscribed;
+    }
+
+    public void setTimeSubscribed(Timestamp timeSubscribed) {
+        this.timeSubscribed = timeSubscribed;
+    }
+}
