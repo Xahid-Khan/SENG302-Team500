de351e317615300809a837cb5deed3851bfd2e99
Shaylin Simadari <ssi162@uclive.ac.nz>
2022-08-24 15:52:05 +1200

    Changed name of HomePageService to SubscriptionService for clarity.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/HomePageController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/HomePageController.java
@@ -2,15 +2,12 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.authentication.PortfolioPrincipal;
 import nz.ac.canterbury.seng302.portfolio.model.contract.SubscriptionContract;
-import nz.ac.canterbury.seng302.portfolio.service.HomePageService;
-import nz.ac.canterbury.seng302.portfolio.service.RolesService;
+import nz.ac.canterbury.seng302.portfolio.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /** Handles the GET request on the /home-page endpoint. */
 @Controller
@@ -18,13 +15,13 @@ import java.util.List;
 public class HomePageController {
 
     @Autowired
-    private HomePageService homePageService;
+    private SubscriptionService subscriptionService;
 
     @PostMapping(value = "/home-page/subscribe", produces = "application/json")
     public ResponseEntity<?> subscribe(@AuthenticationPrincipal PortfolioPrincipal principal,
                                        @RequestBody SubscriptionContract subscription) {
         try{
-            homePageService.subscribe(subscription);
+            subscriptionService.subscribe(subscription);
         } catch (Exception e){
             return ResponseEntity.internalServerError().build();
         }
@@ -35,7 +32,7 @@ public class HomePageController {
     public ResponseEntity<?> unsubscribe(@AuthenticationPrincipal PortfolioPrincipal principal,
                                        @RequestBody SubscriptionContract subscription) {
         try{
-            homePageService.unsubscribe(subscription);
+            subscriptionService.unsubscribe(subscription);
         } catch (Exception e){
             return ResponseEntity.internalServerError().build();
         }
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/SubscriptionService.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/service/SubscriptionService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class HomePageService {
+public class SubscriptionService {
 
     @Autowired
     private SubscriptionRepository subscriptionRepository;
