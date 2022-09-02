5d0f2fda5b59341ca7900b7fa951373704772071
Cody Larsen <cal127@uclive.ac.nz>
2022-09-02 15:37:01 +1200

    Add name and count for high-fives on hover

--- portfolio/src/main/frontend/src/page/group_feed/ShowAllPosts.tsx
+++ portfolio/src/main/frontend/src/page/group_feed/ShowAllPosts.tsx
@@ -30,7 +30,7 @@ export function ShowAllPosts() {
             "name": "John Snow",
             "time": "2022-08-20T10:00:00",
             "content": "This is my first post",
-            "highFives": 5,
+            "highFives": ["James Potter", "Thomas Jefferson", "Lewis Capaldi"],
             "comments": [{
                 "name": "James Potter",
                 "time": "2022-08-20T10:30:00",
@@ -42,7 +42,7 @@ export function ShowAllPosts() {
                 "name": "James Potter",
                 "time": "2022-08-20T12:00:00",
                 "content": "What a wonderful day it is",
-                "highFives": 5,
+                "highFives": ["James Potter", "Jim Bean", "Jeff Winger", "Troy Savant"],
                 "comments": [{
                     "name": "John Snow",
                     "time": "2022-08-20T11:30:00",
@@ -77,11 +77,20 @@ export function ShowAllPosts() {
                     <div className={"post-body"}>{post.content}</div>
                     <div className={"border-line"}/>
                     <div className={"post-footer"}>
-                        <div className={"high-five-overlay"}>
-                            High Five! <span className={"material-icons"}>sign_language</span>
-                        </div>
-                        <div className={"high-five"} id={`high-five-${post.id}`} onClick={() => clickHighFive(post.id)}>
-                            High Five! <span className={"material-icons"}>sign_language</span>
+                        <div className={"high-fives"}>
+                            <div className={"high-five-overlay"}>
+                                High Five! <span className={"material-icons"}>sign_language</span>
+                            </div>
+                            <div className={"high-five"} id={`high-five-${post.id}`} onClick={() => clickHighFive(post.id)}>
+                                High Five! <span className={"material-icons"}>sign_language</span>
+                            </div>
+                            <div className={"high-five-list"}>
+                                <div className={"high-five-count"}><span className={"material-icons"} style={{fontSize: 15}}>sign_language</span>{post['highFives'].length}</div>
+                                <div className={"border-line high-five-separator"}/>
+                                {post['highFives'].map((highFiveName) => (
+                                    <div className={"high-five-names"}>{highFiveName}</div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
