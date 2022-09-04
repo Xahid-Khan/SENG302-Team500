5daaf09174a6f332fe8170335655ae60bb6a8e28
George Carr-Smith <gca73@uclive.ac.nz>
2022-09-05 10:38:38 +1200

    Created the database entity for group repository #feature

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/entity/GroupRepositoryEntity.java
@@ -0,0 +1,73 @@
+package nz.ac.canterbury.seng302.portfolio.model.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+/**
+ * Represents group repositories in the database
+ */
+@Entity
+@Table(name = "repository")
+public class GroupRepositoryEntity extends PortfolioEntity {
+
+    @Column(name = "group_id", nullable = false)
+    private int groupId;
+
+    @Column(name = "repository_id")
+    private int repositoryId;
+
+    @Column(name = "token")
+    private String token;
+
+    protected GroupRepositoryEntity() {
+
+    }
+
+    /**
+     * Creates a new GroupRepositoryEntity with just a group ID
+     * @param groupId The ID of the group associated
+     */
+    public GroupRepositoryEntity(int groupId) {
+        this.groupId = groupId;
+    }
+
+    /**
+     * Creates a new GroupRepositoryEntity with a group ID, token and a repository ID
+     * @param groupId The ID of the group associated
+     * @param repositoryId Groups repository ID
+     * @param token The token used to access the repository
+     */
+    public GroupRepositoryEntity(int groupId, int repositoryId, String token) {
+        this.groupId = groupId;
+        this.repositoryId = repositoryId;
+        this.token = token;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getRepositoryId() {
+        return repositoryId;
+    }
+
+    public void setRepositoryId(int repositoryId) {
+        this.repositoryId = repositoryId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
