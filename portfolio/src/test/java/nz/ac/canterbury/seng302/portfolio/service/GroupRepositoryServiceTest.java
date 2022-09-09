ffaf388fea512fc104e69b5ed1566b6aac3d2441
George Carr-Smith <gca73@uclive.ac.nz>
2022-09-09 20:14:02 +1200

    Attempted to implement tests but was stuck with mocking #test

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupRepositoryServiceTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/service/GroupRepositoryServiceTest.java
@@ -1,16 +1,19 @@
 package nz.ac.canterbury.seng302.portfolio.service;
 
+import nz.ac.canterbury.seng302.portfolio.mapping.GroupRepositoryMapper;
 import nz.ac.canterbury.seng302.portfolio.model.contract.GroupRepositoryContract;
 import nz.ac.canterbury.seng302.portfolio.model.contract.basecontract.BaseGroupRepositoryContract;
 import nz.ac.canterbury.seng302.portfolio.model.entity.GroupRepositoryEntity;
 import nz.ac.canterbury.seng302.portfolio.model.entity.GroupRepositoryRepository;
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,27 +29,36 @@ public class GroupRepositoryServiceTest {
     @Mock
     private GroupRepositoryRepository groupRepositoryRepository;
 
-    private GroupRepositoryContract repo1;
+    @Mock
+    private GroupRepositoryMapper groupRepositoryMapper;
+
+    private GroupRepositoryContract repoContract1;
+    private GroupRepositoryEntity repoEntity1;
     private BaseGroupRepositoryContract emptyRepo1;
     private GroupRepositoryContract repo2;
     private BaseGroupRepositoryContract emptyRepo2;
 
     private List<GroupRepositoryEntity> repoList;
+
+    private List<GroupRepositoryContract> contractList;
     @BeforeEach
     void setup () {
         groupRepositoryRepository.deleteAll();
-        repo1 = new GroupRepositoryContract(1,1,"ABCTOKEN");
-        emptyRepo1 = new BaseGroupRepositoryContract(2);
-        repo2 = new GroupRepositoryContract(5,1340,"BACTOKEN");
-        emptyRepo2 = new BaseGroupRepositoryContract(20);
+        repoContract1 = new GroupRepositoryContract(1,1,"ABCTOKEN");
+        repoEntity1 = new GroupRepositoryEntity(1,1,"ABCTOKEN");
+//        emptyRepo1 = new BaseGroupRepositoryContract(2);
+//        repo2 = new GroupRepositoryContract(5,1340,"BACTOKEN");
+//        emptyRepo2 = new BaseGroupRepositoryContract(20);
         repoList = new ArrayList<>();
-        repoList.add(new GroupRepositoryEntity(repo1.groupId(), repo1.groupId(), repo1.token()));
-        repoList.add(new GroupRepositoryEntity(emptyRepo1.groupId()));
-        repoList.add(new GroupRepositoryEntity(repo2.groupId(), repo2.groupId(), repo2.token()));
-        repoList.add(new GroupRepositoryEntity(emptyRepo2.groupId()));
+        repoList.add(new GroupRepositoryEntity(repoContract1.groupId(), repoContract1.groupId(), repoContract1.token()));
+//        repoList.add(new GroupRepositoryEntity(emptyRepo1.groupId()));
+//        repoList.add(new GroupRepositoryEntity(repo2.groupId(), repo2.groupId(), repo2.token()));
+//        repoList.add(new GroupRepositoryEntity(emptyRepo2.groupId()));
 
         repoList.forEach(repo -> {
             groupRepositoryRepository.save(repo);
+            //convert to contract and add to list
+
         });
     }
 //    /**
@@ -68,26 +80,33 @@ public class GroupRepositoryServiceTest {
 //        var repository = groupRepositoryService.get(Integer.toString(GROUP_ID));
 //    }
 
-    @Test
-    void getAll() {
-//        Mockito.when(groupRepositoryRepository.findAll()).thenReturn(repoList);
-//        List<GroupRepositoryEntity> result= groupRepositoryService.getAll();
-//        assert(result.size() == 4);
+//    @Test
+//    void getAll() {
+//        Mockito.when(groupRepositoryService.getAll()).thenReturn(repoList);
+//
+//
+//
+//        List<GroupRepositoryContract> result= groupRepositoryService.getAll();
+////        assert(result.size() == 4);
 //
 //        for (int i=0; i<result.size(); i++) {
-//            Assertions.assertEquals(repoList.get(i).getId(), result.get(i).getId());;
-//            Assertions.assertEquals(repoList.get(i).getRepositoryID(), result.get(i).getRepositoryID());
-//            Assertions.assertEquals(repoList.get(i).getToken(), result.get(i).getToken());
+//            Assertions.assertEquals(repoList.get(i).getId(), result.get(i).groupId());;
+//            Assertions.assertEquals(repoList.get(i).getRepositoryID(), result.get(i).repositoryId());
+//            Assertions.assertEquals(repoList.get(i).getToken(), result.get(i).token());
 //        }
-    }
+//    }
 
 //    @Test
 //    void add() {
+//        Mockito.when(groupRepositoryRepository.save(repoEntity1)).thenReturn(repoEntity1);
+//        doReturn(repoEntity1).when(groupRepositoryRepository).save(Matchers.any(GroupRepositoryEntity.class));
+//        var result = groupRepositoryService.add(repoContract1.groupId());
+//        assertEquals(repoContract1, result);
 //    }
-//
-//    @Test
-//    void delete() {
-//    }
+
+    @Test
+    void delete() {
+    }
 //
 //    @Test
 //    void update() {
