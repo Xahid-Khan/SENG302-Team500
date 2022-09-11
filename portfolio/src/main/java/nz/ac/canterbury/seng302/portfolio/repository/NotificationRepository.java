1d3c769a35dddbb36fce3e4c56d72093bf6a4901
Shaylin Simadari <ssi162@uclive.ac.nz>
2022-09-11 15:50:51 +1200

    Added a method in NotificationRepository to return a users notifications in chronological order as suggested in MR. #feature

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/repository/NotificationRepository.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/repository/NotificationRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 /**
  * this interface extends the CRUD repository and makes use of the function/methods provided by the library.
  */
-public interface NotificationRepository extends CrudRepository<NotificationEntity, String> {}
+public interface NotificationRepository extends CrudRepository<NotificationEntity, String> {
+    Iterable<NotificationEntity> findAllByUserIdOrderByTimeNotifiedDesc(int userId);
+}
