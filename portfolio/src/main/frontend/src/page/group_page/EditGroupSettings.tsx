8035c06c98cfba5484eba723416c091158ac03d4
George Carr-Smith <gca73@uclive.ac.nz>
2022-09-14 01:09:26 +1200

    Added functionality for submitting group repository information to the backend #feature

--- portfolio/src/main/frontend/src/page/group_page/EditGroupSettings.tsx
+++ portfolio/src/main/frontend/src/page/group_page/EditGroupSettings.tsx
@@ -36,6 +36,7 @@ export function EditGroupSettings( {viewGroupId}: any ) {
     const [alias, setAlias] = React.useState('')
     const[repositoryID, setRepositoryID] = React.useState('')
     const[repositoryName, setRepositoryName] = React.useState('')
+    const[repositoryToken, setRepositoryToken] = React.useState('')
     const [longCharCount, setLongCharCount] = React.useState(0)
 
     const handleCancel = () => {
@@ -43,7 +44,7 @@ export function EditGroupSettings( {viewGroupId}: any ) {
         window.location.reload()
     }
 
-    const validateEditForm = (e: FormEvent) => {
+    const validateLongName = (e: FormEvent) => {
         let errors = false
         let errorMessage
 
@@ -57,7 +58,41 @@ export function EditGroupSettings( {viewGroupId}: any ) {
             document.getElementById("edit-group-error").innerText = errorMessage;
         } else {
             document.getElementById("group-settings-modal-open").style.display = "none"
-            window.location.reload()
+            // window.location.reload()
+        }
+    }
+
+    const validateRepositoryInfo = async (e: FormEvent) => {
+        let errors = false
+        let errorMessage
+
+        if (isNaN(parseInt(repositoryID))) {
+            errors = true
+            errorMessage = "Repository ID must be a number."
+        }
+
+
+        if (errors) {
+            e.preventDefault()
+            document.getElementById("edit-group-error").innerText = errorMessage;
+        } else {
+            var repositoryIDNumber = +repositoryID
+            await fetch(`/groups/update_repository/`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({"groupId": viewGroupId, "repositoryId": repositoryIDNumber,"token":repositoryToken})
+            }).then((res) => {
+                if (res.ok === true) {
+                    // window.location.reload()
+                } else {
+                    document.getElementById("create-post-error").innerText = "You do not have permission to post in this group."
+                }
+            }).catch((e) => {
+                console.log("error ", e)
+            })
+
         }
     }
     const canEdit = (myGroup !== undefined ? myGroup.users.filter((user) => user.id === userId).length > 0 : false) || isStudent === "false"
@@ -78,29 +113,36 @@ export function EditGroupSettings( {viewGroupId}: any ) {
                             {canEdit ? <input type="text" name="long-name" className="input-name" id={"long-name"} placeholder={myGroup.longName} maxLength={64} onChange={(e) => {setLongName(e.target.value); setLongCharCount(e.target.value.length)}}/>
                                 : <label>  {myGroup.longName}</label> }
                             {canEdit ? <span className="input-length" id="long-name-length">{longCharCount} / 64</span> : ""}
+                            { canEdit ?
+                                <div className="modal-buttons">
+                                    <button onClick={(e) => validateLongName(e)} className="button" id="group-settings-confirm">Save</button>
+                                </div>
+                                : ""}
                         </div>
                         <h3>Repo Settings</h3>
+                        <form onSubmit={(e) => validateRepositoryInfo(e)}>
                         <label className={"settings-title"}>Alias:</label>
-                        {canEdit ? <input type="text" name="alias" className="input-name" id={"alias"} maxLength={64} onChange={(e) => {setAlias(e.target.value)}}/>
+                        {canEdit ? <input type="text" name="alias" className="input-name" required id={"alias"} maxLength={64} onChange={(e) => {setAlias(e.target.value)}}/>
                             : <label> Default alias</label>}
                         <div>
                             <label>Repository ID:</label>
-                            {canEdit ? <input type="text" name="alias" className="input-name" id={"alias"} maxLength={64} onChange={(e) => {setRepositoryID(e.target.value)}}/>
+                            {canEdit ? <input type="text" name="repository-id" className="input-name" required id={"repository-id"} maxLength={64} onChange={(e) => {setRepositoryID(e.target.value)}}/>
                                 : <label> Default alias</label>}
                         </div>
                         <div>
                             <label>Token:</label>
-                            {canEdit ? <input type="text" name="alias" className="input-name" id={"alias"} maxLength={64} onChange={(e) => {setRepositoryName(e.target.value)}}/>
+                            {canEdit ? <input type="text" name="repository-token" className="input-name" required id={"repository-token"} maxLength={64} onChange={(e) => {setRepositoryToken(e.target.value)}}/>
                             : <label> Default alias</label>}
-
                         </div>
+
                         <div className="form-error" id="edit-group-error"/>
                         { canEdit ?
                             <div className="modal-buttons">
-                                <button onClick={(e) => validateEditForm(e)} className="button" id="group-settings-confirm">Save</button>
+                                <button className="button" id="group-repository-save" type={"submit"}>Save</button>
                                 <button onClick={() => handleCancel()} className="button" id="group-settings-cancel">Cancel</button>
                             </div>
                             : ""}
+                        </form>
                     </div>
                 </div>
 
