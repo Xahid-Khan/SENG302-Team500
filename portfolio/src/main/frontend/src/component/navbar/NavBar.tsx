f4c2ba437cb800d59722ab2753d4a63bc1e24d22
Andrew Cook <aco155@uclive.ac.nz>
2022-09-30 15:41:38 +1300

    Added controller and script to check token

--- portfolio/src/main/frontend/src/component/navbar/NavBar.tsx
+++ portfolio/src/main/frontend/src/component/navbar/NavBar.tsx
@@ -1,61 +1,55 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {observer} from "mobx-react-lite";
-import {
-    AppBar,
-    Box,
-    Button,
-    Toolbar,
-    Typography
-} from "@mui/material";
+import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
 import {NotificationDropdown} from "../notifications/NotificationDropdown";
 import {AccountDropdown} from "./AccountDropdown";
 import {getAbsolutePath} from "../../util/RelativePathUtil";
-import {Message as StompMessage} from "@stomp/stompjs/esm6/i-message";
-import {Socket} from "../../entry/live_updating";
 import {MessageButton} from "./MessageButton";
 
 export const navigateTo = (page: string) => {
-    const globalUrlPathPrefix = window.localStorage.getItem("globalUrlPathPrefix")
-    window.location.href = getAbsolutePath(globalUrlPathPrefix, page)
+  const globalUrlPathPrefix = window.localStorage.getItem("globalUrlPathPrefix")
+  window.location.href = getAbsolutePath(globalUrlPathPrefix, page)
 }
 
 export const NavBar: React.FC = observer(() => {
 
-    const globalUrlPathPrefix = window.localStorage.getItem("globalUrlPathPrefix")
+  const globalUrlPathPrefix = window.localStorage.getItem("globalUrlPathPrefix")
 
-    return (
-        <React.Fragment>
-            <AppBar position="fixed" sx={{ bgcolor: "#788" }}>
-                <Toolbar>
-                    <Box style={{display: "flex", paddingTop: 8}}>
-                        <img src={`${globalUrlPathPrefix}/images/SprintrLogoTransparent.png`} style={{maxWidth: 30}} alt={"Sprintr Logo"}/>
-                        <Typography
-                            variant="h5">ENG SPRINTR</Typography>
-                    </Box>
+  return (
+      <React.Fragment>
+        <AppBar position="fixed" sx={{bgcolor: "#788"}}>
+          <Toolbar>
+            <Box style={{display: "flex", paddingTop: 8}}>
+              <img src={`${globalUrlPathPrefix}/images/SprintrLogoTransparent.png`}
+                   style={{maxWidth: 30}} alt={"Sprintr Logo"}/>
+              <Typography
+                  variant="h5">ENG SPRINTR</Typography>
+            </Box>
 
-                    <Box sx={{pl: 2, flexGrow: 1}}>
-                        <Button color='inherit' onClick={() => navigateTo("home_feed")}>
-                            <Typography textAlign="center">Home</Typography>
-                        </Button>
-                        <Button color='inherit' onClick={() => navigateTo("project-details")}>
-                            <Typography textAlign="center">Project</Typography>
-                        </Button>
-                        <Button color='inherit' onClick={() => navigateTo("user-list")}>
-                            <Typography textAlign="center">Users</Typography>
-                        </Button>
-                        <Button color='inherit' onClick={() => navigateTo("groups")}>
-                            <Typography textAlign="center">Groups</Typography>
-                        </Button>
-                    </Box>
+            <Box sx={{pl: 2, flexGrow: 1}}>
+              <Button color='inherit' onClick={() => navigateTo("home_feed")}>
+                <Typography textAlign="center">Home</Typography>
+              </Button>
+              <Button color='inherit' onClick={() => navigateTo("project-details")}>
+                <Typography textAlign="center">Project</Typography>
+              </Button>
+              <Button color='inherit' onClick={() => navigateTo("user-list")}>
+                <Typography textAlign="center">Users</Typography>
+              </Button>
+              <Button color='inherit' onClick={() => navigateTo("groups")}>
+                <Typography textAlign="center">Groups</Typography>
+              </Button>
+            </Box>
 
-                    <MessageButton></MessageButton>
-                    <NotificationDropdown></NotificationDropdown>
-                    <AccountDropdown></AccountDropdown>
+            <MessageButton></MessageButton>
+            <NotificationDropdown></NotificationDropdown>
+            <AccountDropdown></AccountDropdown>
 
-                </Toolbar>
-            </AppBar>
-            <Toolbar sx={{mb:3}}/>
-          <script type="text/javascript"></script>
-        </React.Fragment>
-    )
+          </Toolbar>
+        </AppBar>
+        <Toolbar sx={{mb: 3}}/>
+        <script type="text/javascript"
+                src={`${globalUrlPathPrefix}/scripts/token_validator.js`}></script>
+      </React.Fragment>
+  )
 })
