6670c4f626944ee9567a8da50b1992b84a1b1901
Andrew Cook <aco155@uclive.ac.nz>
2022-09-30 14:25:14 +1300

    Added mocking to start fixing the tests

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/AuthorisationParamsHelper.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/AuthorisationParamsHelper.java
@@ -5,12 +5,13 @@ import static org.mockito.ArgumentMatchers.any;
 import java.util.List;
 import java.util.stream.Collectors;
 import nz.ac.canterbury.seng302.portfolio.authentication.PortfolioPrincipal;
-import nz.ac.canterbury.seng302.portfolio.service.UserAccountService;
+import nz.ac.canterbury.seng302.portfolio.controller.AuthenticatedController;
+import nz.ac.canterbury.seng302.portfolio.service.AuthStateService;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
 import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
 import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
 import org.mockito.Mockito;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,9 +21,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
  * It can build a claim around given UserRoles, and can build a claim for one or more UserRole.
  */
 public class AuthorisationParamsHelper {
-
-  @MockBean
-  private static UserAccountService userAccountService;
 
   /**
    * Helper function to build the claim together and configure correctly.
@@ -64,6 +62,16 @@ public class AuthorisationParamsHelper {
    * @param roles a list of UserRoles to add to the claim
    */
   public static void setParams(String name, List<UserRole> roles) {
+    AuthStateService authStateService = Mockito.mock(AuthStateService.class);
+    AuthenticatedController authenticatedController =
+        Mockito.mock(AuthenticatedController.class, Mockito.CALLS_REAL_METHODS);
+    Mockito.when(authenticatedController.getUserId(any())).thenReturn(-1);
+    Mockito.when(authenticatedController.getUser(any()))
+        .thenReturn(UserResponse.newBuilder().addAllRoles(roles).build());
+    Mockito.when(authenticatedController.isTeacher(any()))
+        .thenReturn(roles.contains(UserRole.TEACHER));
+    Mockito.when(authenticatedController.isCourseAdmin(any()))
+        .thenReturn(roles.contains(UserRole.COURSE_ADMINISTRATOR));
     buildClaim(name, roles.stream().map(Object::toString).collect(Collectors.joining(", ")));
   }
 
@@ -74,6 +82,15 @@ public class AuthorisationParamsHelper {
    * @param role the role for the claim to have
    */
   public static void setParams(String name, UserRole role) {
+    AuthStateService authStateService = Mockito.mock(AuthStateService.class);
+    AuthenticatedController authenticatedController =
+        Mockito.mock(AuthenticatedController.class, Mockito.CALLS_REAL_METHODS);
+    Mockito.when(authenticatedController.getUserId(any())).thenReturn(-1);
+    Mockito.when(authenticatedController.getUser(any()))
+        .thenReturn(UserResponse.newBuilder().addAllRoles(List.of(role)).build());
+    Mockito.when(authenticatedController.isTeacher(any())).thenReturn(role == UserRole.TEACHER);
+    Mockito.when(authenticatedController.isCourseAdmin(any()))
+        .thenReturn(role == UserRole.COURSE_ADMINISTRATOR);
     buildClaim(name, role.toString());
   }
 }
